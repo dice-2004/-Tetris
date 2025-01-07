8d90ba492351b44266682a45c3d2e7e3c008ab71
dice-2004 <135799601+dice-2004@users.noreply.github.com>
2025-01-07 16:55:18 +0900

    Refactor Field class to include right and left movement methods

--- src/entities/dataclass/field.py
+++ src/entities/dataclass/field.py
@@ -9,35 +9,37 @@ y=1
 class Field:
     map:List[List[int]] = dataclasses.field(default_factory=lambda:[[0 for _ in range(10)] for _ in range(20)])
 
-    def down(self,possition:List[List[int]],border_y:int) -> None:
+    def down(self,possition:List[List[int]],border_x:int,border_y:int) -> int:
         for block in possition:
             if block[y]+border_y+1 > 19:
-                return
+                return border_y
         for block in possition:
-            self.map[block[y]+border_y][block[x]] = 0
+            self.map[block[y]+border_y][block[x]+border_x] = 0
+        border_y+=1
         for block in possition:
-            self.map[block[y]+1+border_y][block[x]] = 1
+            self.map[block[y]+border_y][block[x]+border_x] = 1
+        return border_y
 
-    def right(self,possition:List[List[int]],border_x:int) -> int:
+    def right(self,possition:List[List[int]],border_x:int,border_y:int) -> int:
         for block in possition:
             if block[x]+border_x+1 > 9:
                 return border_x
 
         for block in possition:
-            self.map[block[y]][block[x]+border_x] = 0
+            self.map[block[y]+border_y][block[x]+border_x] = 0
         border_x+=1
         for block in possition:
-            self.map[block[y]][block[x]+border_x] = 1
+            self.map[block[y]+border_y][block[x]+border_x] = 1
         return border_x
 
-    def left(self,possition:List[List[int]],border_x:int) -> int:
+    def left(self,possition:List[List[int]],border_x:int,border_y:int) -> int:
         for block in possition:
             if block[x]+border_x-1 < 0:
                 return border_x
 
         for block in possition:
-            self.map[block[y]][block[x]+border_x] = 0
+            self.map[block[y]+border_y][block[x]+border_x] = 0
         border_x-=1
         for block in possition:
-            self.map[block[y]][block[x]+border_x] = 1
+            self.map[block[y]+border_y][block[x]+border_x] = 1
         return border_x
--- src/entities/game.py
+++ src/entities/game.py
@@ -11,28 +11,47 @@ class Game:
             self.tetromino = kakin.Kakin().Tetromino
         elif self.mode == 1:
             self.tetromino = default.Defalut().Tetromino
-        self.root = tk.Tk()
         self.MAP:field.Field = field.Field()
 
         with open("src/entities/dataclass/map.txt","w") as f:
             for row in self.MAP.map:
                 f.write(" ".join(map(str, row)) + "\n")
 
+        self.border_x:int = 0
+        self.border_y:int = 0
+
+        self.root:tk.Tk = tk.Tk()
+        self.root.title("Tetris")
+        self.root.geometry("500x500")
+        # self.root.protocol("WM_DELETE_WINDOW", self.exit)
+        self.root.focus_force()
+        self.root.bind("<Down>", self.down)
+        self.root.bind("<Right>", self.right)
+        self.root.bind("<Left>", self.left)
+        self.root.mainloop()
         # データクラスの書き換え
+    def down(self,event) -> None:
+        self.border_y=self.MAP.down(self.tetromino["L"],self.border_x,self.border_y)
+        with open("src/entities/dataclass/map.txt","w") as f:
+            for row in self.MAP.map:
+                f.write(" ".join(map(str, row)) + "\n")
+
+    def right(self,event) -> None:
+        self.border_x=self.MAP.right(self.tetromino["L"],self.border_x,self.border_y)
+        with open("src/entities/dataclass/map.txt","w") as f:
+            for row in self.MAP.map:
+                f.write(" ".join(map(str, row)) + "\n")
+
+    def left(self,event) -> None:
+        self.border_x=self.MAP.left(self.tetromino["L"],self.border_x,self.border_y)
+        with open("src/entities/dataclass/map.txt","w") as f:
+            for row in self.MAP.map:
+                f.write(" ".join(map(str, row)) + "\n")
 
     def fall(self) -> None:
-        border_y:int = 0
-        border_x:int = 0
         string:str=random.choice(list(self.tetromino.keys()))
-        # while True:
-        #     self.MAP.down(self.tetromino[string],border_y)
-        #     border_y+=1
-        #     with open("src/entities/dataclass/map.txt","w") as f:
-        #         for row in self.MAP.map:
-        #             f.write(" ".join(map(str, row)) + "\n")
-        #     sleep(1)
         while True:
-            border_x=self.MAP.right(self.tetromino[string],border_x)
+            self.border_y=self.MAP.down(self.tetromino[string],self.border_y)
 
             with open("src/entities/dataclass/map.txt","w") as f:
                 for row in self.MAP.map:
